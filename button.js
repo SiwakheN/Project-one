74a0888b84a0265a73261971f1acc91ee3eebd82
VERPAKT\shane <shane@goldmedialab.com>
2024-09-15 13:50:07 +0200

    shane change

--- button.js
+++ button.js
@@ -1,6 +1,6 @@
 function subscribe() {
     let name = prompt("What is your name?");
-    prompt("What is your email address?");
+    prompt("shane What is your email address?");
 
     alert(
         "Thank you 😁! We will email you a link so you can book your spot"
